0216e94eab4c4a98d0c0198da5215659d90aeb58
rasmodean <ethanwboss115@gmail.com>
2024-08-27 11:56:13 -0400

    Add game loop to main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill('black')
         pygame.display.flip()
 
