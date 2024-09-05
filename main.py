d08d2df393e74ee1894ddd5da5bfe3c04424dd44
rasmodean <ethanwboss115@gmail.com>
2024-09-05 17:02:33 -0400

    Update containers main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
@@ -15,13 +21,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for u in updatable:
+            u.update(dt)
+
         screen.fill('black')
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         dt = pygame.time.Clock().tick(FRAME_RATE) / 1000
-        player.update(dt)
-
+    
 
 
 if __name__ == "__main__":
