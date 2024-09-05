788ccf2e26ef9b22507c69e181dada7e9250a13e
rasmodean <ethanwboss115@gmail.com>
2024-09-05 16:42:48 -0400

    Add player.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -7,6 +8,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -14,8 +16,11 @@ def main():
                 return
 
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+
         dt = pygame.time.Clock().tick(FRAME_RATE) / 1000
+        player.update(dt)
 
 
 
